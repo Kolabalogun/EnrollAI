6dac5afb6ec26c030457321fff74e9b6c1b3f983
Ibrahim Kolabalogun <32678498+Kolabalogun@users.noreply.github.com>
2024-10-05 08:33:36 +0100

    Update root.tsx

--- src/pages/root.tsx
+++ src/pages/root.tsx
@@ -3,13 +3,13 @@ import { Outlet } from "react-router-dom";
 
 const Root = () => {
   return (
-    <div className="flex   h-[100vh] w-[100%] relative   ">
+    <div className="flex h-[100vh] w-[100%] relative   ">
       {/* Sidebar component for navigation */}
       <Sidebar />
-      <main className="pb-20  remove-scrollbar overflow-y-scroll min-h-screen h-full  flex-1 transition  ">
+      <main className="pb-20  remove-scrollbar overflow-y-scroll min-h-screen h-full flex-1 transition  ">
         {/* Navbar component for the top navigation bar */}
         <Navbar />
-        <div className="  p-3 md:p-6  max-w-[1680px]  mx-auto  ">
+        <div className="p-3 md:p-6  max-w-[1680px]  mx-auto  ">
           {/* Outlet renders the component based on the active route */}
           <Outlet />
         </div>
